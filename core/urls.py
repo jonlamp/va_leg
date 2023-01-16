92bc9f793cdaf9908891771d63b29446e5bad47e
lampski <jonlamp3@gmail.com>
2023-01-16 00:16:08 -0500

    added index and bill view

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path, include
 
 urlpatterns = [
-    path("", view=views.index, name="index")
+    path("", view=views.index, name="index"),
+    path("bill/<int:bill_id>",view=views.bill_view,name="bill_view")
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from core.models import Bill
 
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Present!</h1>")
+    return render(request, 'core/index.html')
+
+def bill_view(request,bill_id):
+    bill = Bill.objects.get(pk=bill_id)
+    context = {
+        'title':bill.bill_number,
+        'bill':bill
+    }
+    return render(request,'core/bill.html',context)
