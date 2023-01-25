866080fcac0e8a78a08e58278de7c449e7853e18
lampski <jonlamp3@gmail.com>
2023-01-25 00:39:51 -0500

    Added advanced search

--- core/views.py
+++ core/views.py
@@ -1,12 +1,36 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from core.models import Bill, BillSummaries, Patron
+from core.models import Bill, BillSummaries, Patron, Session
 from django import forms
-from django.db.models import Q, IntegerField,Min
+from django.db.models import Q, IntegerField,Min, Max
 from django.db.models.functions import Cast,Substr
+from django.forms import ModelChoiceField
+
+class CustomModelChoice(ModelChoiceField):
+    def label_from_instance(self, obj):
+        return obj.title
 
 class BasicSearch(forms.Form):
     query = forms.CharField(label='Search', max_length=100)
+
+class AdvancedSearch(forms.Form):
+    title = forms.CharField(label="Title Contains:", max_length=100,required=False)
+    introduced = forms.IntegerField(
+        label="Year the bill introduced",
+        max_value=Bill.objects.latest('d_introduced').d_introduced.year,
+        min_value=Bill.objects.earliest('d_introduced').d_introduced.year,
+        error_messages={
+            'max_value':'Sorry, our database does not contain that year yet.',
+            'min_value':'Sorry, our database does not contain that year yet.'
+        },
+        required=False
+    )
+    legislator = forms.CharField(label="Legislator name contains:", max_length=40, required=False)
+    summary = forms.CharField(label="Summary contains:", max_length="100",required=False)
+    session = CustomModelChoice(
+        Session.objects.all(),
+        required=False
+    )
 
 # Create your views here.
 def index(request):
@@ -37,23 +61,58 @@ def bill_view(request,bill_id):
 def search(request):
     search_form = BasicSearch()
     search_form.fields['query'].widget.attrs['placeholder'] = 'Search'
-    q = request.GET['query']
-    results = Bill.objects.filter(
-        Q(bill_number__contains=q) |
-        Q(title__contains=q) |
-        Q(summaries__content__contains=q)
-    ).annotate( 
-        min_year=Min('sessions__year')
-    ).order_by(
-        'min_year',
-        Substr('bill_number',1,2),
-        Cast(Substr('bill_number',pos=3),IntegerField())
-    )
-    
-    context = {
-        'title':'VAL - Search Results',
-        'search_form':search_form,
-        'query_string':q,
-        'results':results
-    }
+    if request.method == "GET":
+        if 'query' in request.GET:
+            q = request.GET['query']
+            results = Bill.objects.filter(
+                Q(bill_number__contains=q) |
+                Q(title__contains=q) |
+                Q(summaries__content__contains=q)
+            ).annotate( 
+                min_year=Min('sessions__year')
+            ).order_by(
+                'min_year',
+                Substr('bill_number',1,2),
+                Cast(Substr('bill_number',pos=3),IntegerField())
+            )
+            
+            context = {
+                'title':'VAL - Search Results',
+                'search_form':search_form,
+                'query_string':q,
+                'results':results,
+                'advanced_search_form':AdvancedSearch()
+            }
+        else:
+            context={
+                'title':'VAL - Search Results',
+                'search_form':search_form,
+                'advanced_search_form':AdvancedSearch()
+            }
+    elif request.method == "POST":
+        data = request.POST
+        results = Bill.objects.all()
+        q = ""
+        if len(data['title'])>0:
+            results = results.filter(title__contains=data['title'])
+            q += f"titles containing \"{data['title']}\"; "
+        if len(data['introduced'])>0:
+            results = results.filter(d_introduced__year=data['introduced'])
+            q += f"bills introduced in \"{data['introduced']}\"; "
+        if len(data['legislator'])>0:
+            results = results.filter(patrons__name__contains=data['legislator'])
+            q += f"bills where legislators whose names contain \"{data['legislator']}\" are a patron; "
+        if len(data['summary'])>0:
+            results = results.filter(summaries__content__contains=data['summary'])
+            q += f"bills whose summaries contain \"{data['summary']}\"; "
+        if len(data['session'])>0:
+            results = results.filter(sessions__pk=data['session'])
+            q += f"bills with actions during the {Session.objects.get(pk=data['session']).title}; "
+        context={
+            'title':'VAL - Search Results',
+            'search_form':search_form,
+            'query_string':q,
+            'results':results[:100],
+            'advanced_search_form':AdvancedSearch()
+        }
     return render(request,'core/results.html',context)
--- legislation/settings.py
+++ legislation/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-from secret_config import SECRET, STATIC_RT
+from secret_config import SECRET, STATIC_RT, LOCAL_DEBUG
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = SECRET
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = LOCAL_DEBUG
 
 ALLOWED_HOSTS = [
     "jonlamp.pythonanywhere.com",
